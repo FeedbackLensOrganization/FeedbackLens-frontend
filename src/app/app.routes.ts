d5543cb0ea797985cc1450365732477013fe511c
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-17 14:41:09 +0100

    Preparation of the feedback page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+    // ── Feedback ──
+    path: '',
+    loadComponent: () =>
+      import('./features/public/feedback/feedback').then(
+        (m) => m.Feedback
+      ),
+    title: 'Feedback',
+  },
+];
