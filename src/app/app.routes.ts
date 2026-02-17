92d21bbd16456b84a7a49f7e6483f5d77bb9bc06
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-17 14:54:34 +0100

    Preparation of the auth page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-      {
+  {
     // ── Feedback ──
     path: '',
-    loadComponent: () =>
-      import('./features/public/feedback/feedback').then(
-        (m) => m.Feedback
-      ),
+    loadComponent: () => import('./features/public/feedback/feedback').then((m) => m.Feedback),
     title: 'Feedback',
+  },
+  {
+    // ── Auth ────────────────────────────────────────────────────────
+    path: 'login',
+    loadComponent: () =>
+      import('./features/public/auth/auth').then((m) => m.Auth),
+    title: 'FeedbackLens – Login',
   },
 ];
