9bf0a3aee421e97b038f1268dfe76f63706eb9dc
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-18 05:55:14 +0100

    enviroments installed

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,7 @@
+import { EnvironmentInterface } from "./core/environment.interface";
+
+export const environment: EnvironmentInterface = {
+    production: false,
+    apiUrl: "http://localhost:9000/api/v1",
+    version: "1.0",
+}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+import { EnvironmentInterface } from "./core/environment.interface";
+
+export const environment: EnvironmentInterface = {
+    production: true,
+    apiUrl: "http://localhost:9000/api/v1",
+    version: "1.0",
+}
