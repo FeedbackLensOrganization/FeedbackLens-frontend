bac86de834586920bb081716f553aaa0cc939d1c
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-19 12:46:46 +0100

    Load immediately feedbacks

--- src/app/features/admin-dashboard/admin-dashboard.ts
+++ src/app/features/admin-dashboard/admin-dashboard.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ApiService } from '../../config/api-service';
 import { Feedback } from './model/feedback-module';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -9,12 +11,21 @@ import { Feedback } from './model/feedback-module';
   styleUrl: './admin-dashboard.css',
 })
 export class AdminDashboard {
+  private apiService = inject(ApiService);
 
-   feedbacks: Feedback[] = [];
-   constructor(private apiService :ApiService){
-   apiService.getFeedbacks().subscribe(res => {
-    console.log(res);
-      this.feedbacks = res;
-    });
-   }
+  error = signal<string | null>(null);
+
+  constructor() {
+    this.feedbacks();
+  }
+
+  feedbacks = toSignal(
+    this.apiService.getFeedbacks().pipe(
+      catchError((err) => {
+        this.error.set('Fehler beim Laden');
+        return of([]);
+      }),
+    ),
+    { initialValue: [] },
+  );
 }
