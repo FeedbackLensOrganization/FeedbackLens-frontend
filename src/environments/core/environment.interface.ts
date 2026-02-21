b7cdf83750c67f11f44868f683eb7683fb8d8436
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-21 05:39:29 +0100

    benötigte AWS Cognito  configuration

--- src/environments/core/environment.interface.ts
+++ src/environments/core/environment.interface.ts
@@ -2,4 +2,11 @@ export interface EnvironmentInterface {
   production: boolean;
   apiUrl: string;
   version: string;
+
+  // AWS Cognito configuration
+  authorityAWS: string;
+  redirectUrlAWS: string;
+  clientIdAWS: string;
+  scopeAWS: string;
+  responseTypeAWS: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,4 +4,9 @@ export const environment: EnvironmentInterface = {
     production: true,
     apiUrl: "http://localhost:9000/api/v1",
     version: "1.0",
+    authorityAWS: "",
+    redirectUrlAWS: "",
+    clientIdAWS: "",
+    scopeAWS: "",
+    responseTypeAWS: ""
 }
