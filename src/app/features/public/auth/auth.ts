de19fcc72f5a3ffa4e59767455bb471b8fffc542
Sebastian <93546847+harry05555@users.noreply.github.com>
2026-02-23 15:47:18 +0100

    login versuch

--- src/app/features/public/auth/auth.ts
+++ src/app/features/public/auth/auth.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/auth/auth.service';
+
 
 @Component({
   selector: 'app-auth',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './auth.html',
   styleUrl: './auth.css',
 })
 export class Auth {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  username = '';
+  password = '';
+  errorMsg = '';
+  loading = false;
+
+  login() {
+    this.errorMsg = '';
+    this.loading = true;
+
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        this.authService.saveToken(res.accessToken);
+        this.router.navigate(['/admindashboard']);
+      },
+      error: () => {
+        this.errorMsg = 'Falsches Passwort. Bitte erneut versuchen.';
+        this.loading = false;
+      }
+    });
+  }
 }
